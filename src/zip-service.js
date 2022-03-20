1211091bdc3bda064db5f554b3b9a2225280d1ed
Hans Schroeder <hajschroeder@gmail.com>
2022-03-20 09:40:44 -0700

    insert missing semi colon

--- src/zip-service.js
+++ src/zip-service.js
@@ -1,17 +1,15 @@
 export default class ZipService {  
   static getZip(zip) {
-    return new Promise(function(resolve, reject) {
-      let request = new XMLHttpRequest();
-      const url = `http://api.openweathermap.org/geo/1.0/zip?zip=${zip},US&appid=${process.env.API_KEY1}`;
-      request.onload = function() {
-        if (this.status === 200) {
-          resolve(request.response);
-        } else {
-          reject(request.response);
+    return fetch(`http://api.openweathermap.org/geo/1.0/zip?zip=${zip},US&appid=${process.env.API_KEY1}`)
+      .then(function(response) {
+        if (!response.ok) {
+          throw Error(response.statusText);
         }
-      };
-      request.open("GET", url, true);
-      request.send();
-    });
+        return response.json();
+      })
+      .catch(function(error) {
+        return error;
+      })
   }
 }
+    
